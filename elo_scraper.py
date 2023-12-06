0d3aec65fb4a459be8a0cb648cded372ab2bb501
Polonez1 <darjus.vasiukevic@gmail.com>
2023-12-06 06:41:26 +0200

    updated

--- elo_scraper.py
+++ elo_scraper.py
@@ -2,15 +2,34 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 import processing_data
 import json
+import pandas as pd
 
 
 class EloParser:
     def __init__(self):
         self.url: str = "http://elofootball.com/"
         self.country_hrefs: dict = {}
-        self.competition_data: dict = {"data": []}
+        self.competition_data: dict = {}
         self.ranking_data: dict = {}
         self.matches_data: dict = {}
+
+    def __get_season_string(self) -> str:
+        season = (
+            self.page.get_by_role("heading", name="Selected season:")
+            .inner_text()
+            .partition("Selected season:")[2]
+            .strip()
+        )
+        return season
+
+    def __append_data(
+        self, df: pd.DataFrame, append_dict: dict, country: str, season: str
+    ):
+        json_data = df.to_json(orient="records", indent=1)
+        json_data = json.loads(json_data)
+        append_dict[country] = {}
+        append_dict[country][season] = {}
+        append_dict[country][season] = json_data
 
     def _season_hrefs_collector(self):
         pass
@@ -29,41 +48,75 @@ class EloParser:
                     self.country_hrefs[country] = href
 
     # ------------------------------------------------------------------------------------------------------#
-    def __collect_competition_data(self):
+    def __get_table_by_nr(self, table_index: int) -> list:
+        """find table by index and return table components
+
+        Args:
+            table_index (int): table place
+
+        Returns:
+            list: headers, rows
+        """
         table_element = self.page.query_selector_all(".sortable.fixed.primary")
-        table = table_element[0]
+        table = table_element[table_index]
         table_data = table.evaluate(
             "(table) => { return { headers: Array.from(table.tHead.rows[0].cells, cell => cell.innerText.trim()), rows: Array.from(table.tBodies[0].rows, row => Array.from(row.cells, cell => cell.innerText.trim())) }; }"
         )
         headers = table_data.get("headers")
         rows = table_data.get("rows")
+
+        return headers, rows
+
+    def __collect_competition_data(self, season: str, country: str) -> None:
+        headers, rows = self.__get_table_by_nr(table_index=0)
+        df = processing_data.transform_competition_data(rows=rows, columns=headers)
+        self.__append_data(
+            df=df, append_dict=self.competition_data, season=season, country=country
+        )
+
+        with open("competition_data.json", "w", encoding="utf-8") as json_file:
+            json.dump(elo.competition_data, json_file, ensure_ascii=False, indent=2)
+
+    def __collect_raking_data(self, season: str, country: str) -> None:
+        headers, rows = self.__get_table_by_nr(table_index=2)
+        df = processing_data.transform_raking_data(columns=headers, rows=rows)
+        self.__append_data(
+            df=df, append_dict=self.ranking_data, season=season, country=country
+        )
+
+        with open("raking_data.json", "w", encoding="utf-8") as json_file:
+            json.dump(elo.ranking_data, json_file, ensure_ascii=False, indent=2)
+
+    def __collect_matches_data(self, season: str, country: str) -> None:
+        headers, rows = self.__get_table_by_nr(table_index=4)
         data_dict = {"headers": headers, "rows": rows}
-        json_data = json.dumps(data_dict, ensure_ascii=False, indent=2)
+        df = processing_data.transform_matches_data(columns=headers, rows=rows)
+        self.__append_data(
+            df=df, append_dict=self.matches_data, season=season, country=country
+        )
 
-        self.competition_data["data"] = json_data
-
-    def __collect_raking_data(self):
-        pass
-
-    def __collect_matches_data(self):
-        pass
+        with open("matches_data.json", "w", encoding="utf-8") as json_file:
+            json.dump(elo.matches_data, json_file, ensure_ascii=False, indent=2)
 
     def __collect_elo_data(self, hrefs: dict):
         for i in hrefs.items():
             country = i[0]
             href = i[1]
             url = self.url + href
-            self.page.goto(url)
-            self.__collect_competition_data()
+            self.page.goto(url, timeout=60000)
+            season = self.__get_season_string()
+            self.__collect_competition_data(season=season, country=country)
+            self.__collect_raking_data(season=season, country=country)
+            self.__collect_matches_data(season=season, country=country)
 
-            break
+            # break
 
     def parse(self):
         with sync_playwright() as playwright:
             browser = playwright.chromium.launch(headless=False)
             self.context = browser.new_context()
             self.page = self.context.new_page()
-            self.page.goto(self.url)
+            self.page.goto(self.url, timeout=60000)
             self.__collect_country_hrefs()
             self.__collect_elo_data(hrefs=self.country_hrefs)
 
@@ -71,6 +124,8 @@ class EloParser:
 if "__main__" == __name__:
     elo = EloParser()
     elo.parse()
-    print(elo.competition_data)
+
 
 # powershell -ExecutionPolicy Bypass -File C:\iLegion\football_elo_scraper\venv\Scripts\Activate.ps1
+# Set-ExecutionPolicy Bypass -Scope Process
+# .\venv\Scripts\activate
--- processing_data.py
+++ processing_data.py
@@ -1,2 +1,146 @@
-def get_season(string: str):
-    pass
+import pandas as pd
+import numpy as np
+
+
+def __rename_competition(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.rename(
+        columns={
+            "Competition": "competition",
+            "Category": "category",
+            "Clubs": "clubs",
+            "Total:\nP": "MP",
+            "W": "Win",
+            "D": "Draw",
+            "L": "Loses",
+            "GF|⌀": "GF",
+            "GA|⌀": "GA",
+            "G|⌀": "G",
+            "ΔG|⌀": "GD",
+            "⌀ Rating": "Rating",
+            "StdDev\nin %": "StdDev",
+        },
+        inplace=False,
+    )
+
+    return df
+
+
+def __split_competition_avg(df: pd.DataFrame) -> pd.DataFrame:
+    df["GF_avg"] = df["GF"].str.partition("|")[2].str.strip()
+    df["GF"] = df["GF"].str.partition("|")[0].str.strip()
+    df["GA_avg"] = df["GA"].str.partition("|")[2].str.strip()
+    df["GA"] = df["GA"].str.partition("|")[0].str.strip()
+    df["G_avg"] = df["G"].str.partition("|")[2].str.strip()
+    df["G"] = df["G"].str.partition("|")[0].str.strip()
+    df["GD_avg"] = df["GD"].str.partition("|")[2].str.strip()
+    df["GD"] = df["GD"].str.partition("|")[0].str.strip()
+
+    return df
+
+
+def transform_competition_data(columns, rows):
+    df = pd.DataFrame(rows, columns=columns)
+    df = __rename_competition(df)
+    df = __split_competition_avg(df)
+
+    return df
+
+
+# ----------------------------------------------------------------------------------------------#
+def __rename_raking_col(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.rename(
+        columns={
+            "#": "Nr",
+            "Club": "team",
+            "Form (last 6)": "form1",
+            "Rating": "rating",
+            "Record:\nSeason": "season_record",
+            "All time": "all_time",
+            "+/-:\n1M": "1m",
+            "1Y": "1y",
+            "Season-to-date +/-:\nTotal": "total",
+        },
+        inplace=False,
+    )
+    col_to_changes = {
+        3: "form2",
+        4: "form3",
+        5: "form4",
+        6: "form5",
+        7: "form6",
+    }
+    for index, new_name in col_to_changes.items():
+        df.columns.values[index] = new_name
+
+    return df
+
+
+def transform_raking_data(columns, rows) -> pd.DataFrame:
+    df = pd.DataFrame(rows, columns=columns)
+    df = __rename_raking_col(df)
+
+    return df
+
+
+# ----------------------------------------------------------------------------------------------------#
+def __rename_matches_col(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.rename(
+        columns={
+            "Date": "date",
+            "Competition": "competition",
+            "Home": "home_team",
+            "Result": "result",
+            "Probabilities:\nH": "home_prb",
+            "D": "draw_prb",
+            "A": "away_prb",
+            "Away": "away_team",
+        },
+        inplace=False,
+    )
+
+    df.columns.values[3] = "home_elo_changed"
+    df.columns.values[9] = "away_elo_changed"
+
+    return df
+
+
+def __replace_empty_with_none(value):
+    return None if value == "" else value
+
+
+def __check_info(value):
+    return None if value == "Info" else value
+
+
+def __split_goals(df: pd.DataFrame) -> pd.DataFrame:
+    df["result"] = df["result"].apply(lambda x: __check_info(x))
+    df["home_goals"] = df["result"].str.partition("-")[0].str.strip()
+    df["away_goals"] = df["result"].str.partition("-")[2].str.strip()
+
+    return df
+
+
+def __add_match_result(df: pd.DataFrame) -> pd.DataFrame:
+    df["match_result"] = np.where(
+        df["result"].isna(),
+        None,
+        np.where(
+            df["home_goals"] > df["away_goals"],
+            "home_win",
+            np.where(df["home_goals"] < df["away_goals"], "away_win", "draw"),
+        ),
+    )
+
+    return df
+
+
+def transform_matches_data(columns, rows) -> pd.DataFrame:
+    df = pd.DataFrame(rows, columns=columns)
+    df["Date"] = df["Date"].apply(lambda x: __replace_empty_with_none(x))
+    df["Date"] = df["Date"].ffill()
+    df = df.drop(columns=df.columns[df.columns == ""])
+    df = __rename_matches_col(df)
+    df = __split_goals(df)
+    df = __add_match_result(df)
+
+    return df
