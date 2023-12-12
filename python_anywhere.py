9ef4c41bad4015e174413ba58cbc02b683861381
Polonez1 <darjus.vasiukevic@gmail.com>
2023-12-12 13:08:03 +0200

    added ssh connector

--- python_anywhere.py
+++ python_anywhere.py
@@ -2,6 +2,8 @@ import sshtunnel
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
+sshtunnel.SSH_TIMEOUT = 5.0
+sshtunnel.TUNNEL_TIMEOUT = 5.0
 
 tunnel = sshtunnel.SSHTunnelForwarder(
     ("ssh.pythonanywhere.com"),
@@ -14,10 +16,13 @@ tunnel.start()
 if tunnel.is_active:
     print("Tunnel is active and successfully established.")
 
+print(1)
 engine = create_engine(
     f"mysql+mysqlconnector://Polonez:lacosanostra@127.0.0.1:{tunnel.local_bind_port}/Polonez$default"
 )
+print(2)
 connection = engine.connect()
+print(3)
 result = connection.execute(text("SELECT 1"))
 for row in result:
     print(row)
--- sql_integration.py
+++ sql_integration.py
@@ -4,18 +4,28 @@ import pandas as pd
 sys.path.append("./SQL-Data-Load/")
 
 import pySQL
+import conn
 
 tables = ["dbo.elo_competition", "dbo.elo_raking", "dbo.elo_matches"]
 
 
 class EloDataLoad:
     def __init__(self):
+        ssh = pySQL.SSHtunnel(
+            ssh_host=conn.ssh_host,
+            ssh_username=conn.ssh_username,
+            ssh_password=conn.ssh_password,
+            remote_bind_address=conn.remote_bind_address,
+        )
+        tunnel = ssh.create_tunnel()
+        tunnel.start()
         self.sql = pySQL.SQL(
-            host="localhost",
-            database="testDB",
-            # user="user",
-            # password="pass",
-            connect_type="MsSQL",
+            host=conn.host,
+            database=conn.database,
+            user=conn.user,
+            password=conn.password,
+            port=tunnel.local_bind_port,
+            connect_type="MySQL",
         )
 
     def load_data(self, df, table_name, truncate: bool):
@@ -34,3 +44,27 @@ class EloDataLoad:
 if "__main__" == __name__:
     sql = EloDataLoad()
     # commit
+
+    # ssh = SSHtunnel(
+    #    ssh_host="ssh.pythonanywhere.com",
+    #    ssh_username="Polonez",
+    #    ssh_password="Lacosanostra1#",
+    #    remote_bind_address=("Polonez.mysql.pythonanywhere-services.com", 3306),
+    # )
+    # tunnel = ssh.create_tunnel()
+    # tunnel.start()
+#
+# mysql = SQL(
+#    host="127.0.0.1",
+#    database="Polonez$default",
+#    user="Polonez",
+#    password="lacosanostra",
+#    port=tunnel.local_bind_port,
+#    connect_type="MySQL",
+# )
+# df = pd.DataFrame({"id": [1, 2, 3], "value": ["a", "b", "c"]})
+# mysql.load_data_to_SQL(df=df, table="test2")
+#
+## df = mysql.get_data(table="test")
+## print(df)
+# tunnel.close()
